300ac3d98aa105c0d2c7f83f30e8344e7fefc110
joshua <joshuabelay@gmail.com>
2025-11-16 12:26:15 +0300

    added navbar and navitems, added manual tailwindconfig

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import { Button } from '@/components/ui/button'
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+      <h1 className="text-2xl underline">W to SaaS ApP</h1>
+      <Button>Let's get Started</Button>
+
+    </div>
   )
 }
 
